8643667575373dedbad212b5e445e1c7df9739c3
Edgar Diaz <ed35376@gmail.com>
2023-04-17 17:41:57 -0400

    Fuction added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the','our', 'his'];
+let adj = ['great', 'big', 'ugly', 'pretty' ];
+let noun = ['jogger','racoon'];
+let domain=['.com', '.net', '.org', 'us', 'io']
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function ramdomURL(a, b, c, d)
+{
+  for(i=0; i<a.length; i++){
+    for(j=0; j<b.length; j++){
+      for(k=0; k<c.length; k++){
+        for(l=0; l<d.length; l++){
+          console.log(a[i]+b[j]+c[k]+d[l])
+        }
+      }
+    }
+  }
+}
+ramdomURL(pronoun, adj, noun, domain)
